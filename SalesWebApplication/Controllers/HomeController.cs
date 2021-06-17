c525b0e0663399db4006869948b31ccb733c2fc8
Felipe Rampazo <felipe_a_r@hotmail.com>
2021-06-17 11:45:56 -0300

    First Model-Controller-View - Department

--- SalesWebApplication/Controllers/HomeController.cs
+++ SalesWebApplication/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using SalesWebApplication.Models;
+using SalesWebApplication.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
