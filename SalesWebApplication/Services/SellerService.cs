5bba69d51e55532fbaac7d24b41fa7f558c01f7a
Felipe Rampazo <felipe_a_r@hotmail.com>
2021-06-30 09:35:30 -0300

    Foreign key not null

--- SalesWebApplication/Services/SellerService.cs
+++ SalesWebApplication/Services/SellerService.cs
@@ -23,8 +23,10 @@ namespace SalesWebApplication.Services
 
         public void Insert(Seller obj)
         {
+            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
+          
         }
 
     }
