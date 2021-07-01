95fe61739c15b32b39f81e971689669bedb8f7c2
Felipe Rampazo <felipe_a_r@hotmail.com>
2021-07-01 10:23:06 -0300

    returning error page

--- SalesWebApplication/Services/SellerService.cs
+++ SalesWebApplication/Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using SalesWebApplication.Services.Exceptions;
 
 namespace SalesWebApplication.Services
 {
@@ -12,26 +13,27 @@ namespace SalesWebApplication.Services
     {
         private readonly SalesWebApplicationContext _context;
 
-        public SellerService (SalesWebApplicationContext context)
+        public SellerService(SalesWebApplicationContext context)
         {
             _context = context;
 
         }
 
-        public List<Seller> FindAll() {
-           return _context.Seller.ToList();
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
         }
 
         public void Insert(Seller obj)
         {
             _context.Add(obj);
             _context.SaveChanges();
-          
+
         }
 
         public Seller FindById(int id)
         {
-            return _context.Seller.Include(obj=> obj.Department).FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Revome(int id)
@@ -41,5 +43,22 @@ namespace SalesWebApplication.Services
             _context.SaveChanges();
         }
 
+        public void Update(Seller obj)
+        {
+            if (!_context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+
+        }
     }
 }
